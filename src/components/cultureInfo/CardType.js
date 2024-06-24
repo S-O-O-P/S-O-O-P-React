4fa9e535c3133754e62a1d30ba60e076b2e71101
NJH <dldks1212@gmail.com>
2024-06-24 23:23:25 +0900

    connect link to cultureDetail.js

--- src/components/cultureInfo/CardType.js
+++ src/components/cultureInfo/CardType.js
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import styles from '../../pages/cultureInfo/CultureInfo.module.css';
 
 export default function CardType(){
   return(
     <>
       <li>
-        <a href="#">
+        <Link to="/cultureinfo/detail">
           <div className={styles.culture_img}>
             <img src="https://mblogthumb-phinf.pstatic.net/MjAyMTA4MTJfOTQg/MDAxNjI4Njk0OTYyMTM2.g23XzZHsRSK-BZgrg6miMxnPTqFahaLqU_DMHKMIyAkg.XU7c5GDWLlfrFS_RwyUXFRolMj2sx4WxOu8op7Cz5Pgg.JPEG.dys99189/1628694898886.jpg?type=w800" alt="culture poster" />
             <span className={styles.culture_mark}>마감임박</span>
@@ -15,10 +16,10 @@ export default function CardType(){
             <p className={styles.early_end}></p>
             <p className={styles.culture_price}><span className={styles.sale_rate}>30%</span> 84,700원</p>
           </div>
-        </a>
+        </Link>
       </li>
       <li>
-        <a href="#">
+        <Link to="/cultureinfo/detail">
           <div className={styles.culture_img}>
             <img src="https://images.pexels.com/photos/26076858/pexels-photo-26076858.jpeg?auto=compress&cs=tinysrgb&w=1200" alt="culture poster" />
             <span className={styles.culture_mark}>마감임박</span>
@@ -29,10 +30,10 @@ export default function CardType(){
             <p className={styles.early_end}></p>
             <p className={styles.culture_price}><span className={styles.sale_rate}>30%</span> 84,700원</p>
           </div>
-        </a>
+        </Link>
       </li>
       <li>
-        <a href="#">
+        <Link to="/cultureinfo/detail">
           <div className={styles.culture_img}>
             <img src="https://images.pexels.com/photos/6899772/pexels-photo-6899772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="culture poster" />
             <span className={styles.culture_mark}>마감임박</span>
@@ -43,10 +44,10 @@ export default function CardType(){
             <p className={styles.early_end}></p>
             <p className={styles.culture_price}><span className={styles.sale_rate}>30%</span> 84,700원</p>
           </div>
-        </a>
+        </Link>
       </li>
       <li>
-        <a href="#">
+        <Link to="/cultureinfo/detail">
           <div className={styles.culture_img}>
             <img src="https://images.pexels.com/photos/4722568/pexels-photo-4722568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="culture poster" />
             <span className={styles.culture_mark}>마감임박</span>
@@ -57,10 +58,10 @@ export default function CardType(){
             <p className={styles.early_end}></p>
             <p className={styles.culture_price}><span className={styles.sale_rate}>30%</span> 84,700원</p>
           </div>
-        </a>
+        </Link>
       </li>
       <li>
-        <a href="#">
+        <Link to="/cultureinfo/detail">
           <div className={styles.culture_img}>
             <img src="https://images.pexels.com/photos/6896326/pexels-photo-6896326.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="culture poster" />
             <span className={styles.culture_mark}>마감임박</span>
@@ -71,13 +72,13 @@ export default function CardType(){
             <p className={styles.early_end}></p>
             <p className={styles.culture_price}><span className={styles.sale_rate}>30%</span> 84,700원</p>
           </div>
-        </a>
+        </Link>
       </li>
     </>
   );
 }
 {/* <li>
-  <a href="#">
+  <Link to="/cultureinfo/detail">
     <div className={styles.culture_img}>
       <img src={`${process.env.PUBLIC_URL}/images/cultureInfo/KakaoTalk_20240524_000933916_04.png`} alt="culture poster" />
       <span className={styles.culture_mark}>마감임박</span>
@@ -88,5 +89,5 @@ export default function CardType(){
       <p className={styles.early_end}></p>
       <p className={styles.culture_price}><span className={styles.sale_rate}>30%</span> 84,700원</p>
     </div>
-  </a>
+  </Link>
 </li> */}
