034ba8b1b3f969212b8d8b7b10114f68f5533c2c
lmcwul <cksdnf90@gmail.com>
2024-06-26 13:19:58 +0900

    # DELETE

--- src/App.js
+++ src/App.js
@@ -9,16 +9,10 @@ import Faq from './pages/serviceCenter/Faq';
 import Notice from './pages/serviceCenter/Notice';
 import Inquiry from './pages/serviceCenter/Inquiry';
 import Main from './pages/main/Main';
-import Honey from './pages/socializing/Honey';
 import CultureInfo from './pages/cultureInfo/CultureInfo';
 import CompletedPage from './pages/login/CompletedPage'
-import Detail from './pages/socializing/Detail';
 import NoticeDetailPage from './pages/serviceCenter/NoticeDetail';
 import CultureDetail from './pages/cultureInfo/CultureDetail';
-import HoneyWrite from './pages/socializing/HoneyWrite';
-import HoneyWrite2 from './pages/socializing/HoneyWrite2';
-import HoneyWrite3 from './pages/socializing/HoneyWrite3';
-import DoneResult from './pages/socializing/DoneResult';
 
 export default function App() {
 
@@ -41,12 +35,6 @@ export default function App() {
               <Route path='/notice' element={<Notice />} /> {/* 공지사항 */}
               <Route path='/inquiry' element={<Inquiry />} /> {/* 1:1문의 */}
               <Route path='/noticedetail' element={<NoticeDetailPage />} /> {/*공지사항 상세페이지*/}
-              <Route path='/honey' element={<Honey/>}/> {/* 허니팟 */}
-              <Route path='/detail' element={<Detail/>}/> {/*허니팟 상세페이지*/}
-              <Route path='/write-honey/step1' element={<HoneyWrite/>}/> {/* 허니팟 글쓰기페이지*/}
-              <Route path='/write-honey/step2' element={<HoneyWrite2/>}/> {/* 허니팟 글쓰기페이지*/}
-              <Route path='/write-honey/step3' element={<HoneyWrite3/>}/> {/* 허니팟 글쓰기페이지*/}
-              <Route path='/result' element={<DoneResult/>}/> {/* 결과페이지 */} {/* useNavigate 사용시, state로 action 값에 담아 전달하면 됩니다.*/}
             </Route> 
             {/* 레이아웃 클로즈 */}              
           </Routes>
