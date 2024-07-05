29cf38d223f9ab7928f5a8e31179a38765026067
LEEMINKUK <158013587+LEEMINKUK@users.noreply.github.com>
2024-07-05 11:30:58 +0900

    Revert "[FEATURE][CULTUREINFO][API]공연/전시 정보 메인 페이지 내 필터링 기능 추가(진행중)"

--- src/pages/cultureInfo/CultureInfo.js
+++ src/pages/cultureInfo/CultureInfo.js
@@ -23,8 +23,8 @@ export default function CultureInfo(props) {
   const [earlyCount, setEarlyCount] = useState(0); // 얼리버드 갯수
   //const [searchValue, setSearchValue] = useState(null) // 검색어
   const [genreCategory, setGenreCategory] = useState("all") // 장르 카테고리  - 전체로 초기화
-  const [subFilter, setSubFilter] = useState("마감임박순") // 최신 등록순 필터로 초기화
-  const [areaFilter, setAreaFilter] = useState("전체 지역") // 지역 필터 - 전체 지역으로 초기화
+  const [subFilter, setSubFilter] = useState("lately") // 최신 등록순 필터로 초기화
+  const [areaFilter, setAreaFilter] = useState("all") // 지역 필터 - 전체 지역으로 초기화
 
   // 페이지네이션 상태 추가
   const [currentPage, setCurrentPage] = useState(1);
@@ -42,8 +42,7 @@ export default function CultureInfo(props) {
       if (props.cultureList) { // api정보 정상적으로 불러오면   
         const cultureListObj = JSON.parse(props.cultureList); // cultureList를 JavaScript 객체로 변환
         // const cultureListObj = (JSON.parse(props.cultureList)).perforList.filter(item => item.realmName === genre || item.title.match("전시")); // cultureList를 JavaScript 객체로 변환
-        cultureListObj.perforList.sort((a,b) => {return Number(a.endDate) - Number(b.endDate)}) // 마감임박순으로 초기화
-        
+        console.log("마감임박순으로 sorted : " + JSON.stringify(cultureListObj.perforList.sort((a,b) => {return Number(a.endDate) - Number(b.endDate)})));
         const totalCount = cultureListObj.totalCount; // totalCount 값 가져오기
         console.log("totalCount: ", totalCount);
         setTotalCount(totalCount);
@@ -83,12 +82,17 @@ export default function CultureInfo(props) {
 
         genreFilterList.forEach(genreBtn => { // 공연/전시 대분류 카테고리 버튼 각각에
           genreBtn.addEventListener('click', (e) => { //클릭 이벤트 추가
-            const genre = e.currentTarget.dataset.genre; // 클릭한 li 태그에 지정한 data-genre 사용자 지정 데이터 특성 가져오기
-            setGenreCategory(genre);
-
-            //카테고리 버튼 클릭시 마다, 하단 필터 초기화
-            setSubFilter("마감임박순");
-            setAreaFilter("전체 지역");
+            //   console.log("e.currentTarget.dataset.genre : " + e.currentTarget.dataset.genre);
+            //   genreFilterList.forEach(item => {
+            //     item.classList.remove(`${styles.active}`);
+            //   });
+            //   const filteredList = cultureList.perforList.filter(item => (item.realmName.match("미술")));
+            //   console.log(filteredList);
+            //   setCultureList(filteredList);
+            //   console.log(e.currentTarget.innerText);
+            //   e.currentTarget.classList.add(`${styles.active}`);
+            // })
+            const genre = e.currentTarget.dataset.genre;
 
             // 모든 버튼에서 active 클래스 제거
             genreFilterList.forEach(item => {
@@ -124,91 +128,45 @@ export default function CultureInfo(props) {
       // 공연/전시 세부 필터링 리스트 아이템
       const detailFilterItem = document.querySelectorAll(`.${styles.detail_filter_list} > li > ul > li`);
 
-      // detailFilter.forEach(detailBtn => { //디테일 필터 각각에
-      //   detailBtn.addEventListener('click', (e) => { // 클릭 이벤트 추가
-      //     e.currentTarget.classList.contains(`${styles.active}`) ? e.currentTarget.classList.remove(`${styles.active}`) : e.currentTarget.classList.add(`${styles.active}`); // 클릭한 버튼의 활성화 여부에 따라 active 클래스 추가 또는 삭제
-      //     e.currentTarget.childNodes[1].classList.contains(`${styles.active}`) ? e.currentTarget.childNodes[1].classList.remove(`${styles.active}`) : e.currentTarget.childNodes[1].classList.add(`${styles.active}`); // 클릭한 버튼 > 필터링 리스트의 active 클래스 추가 또는 삭제
-      //   })
-      // });
+      // 선택한 공연/전시 세부 필터링 리스트 아이템 텍스트
+      // 왼쪽 필터링 리스트 선택 필터
+      const selectedLeftOption = document.querySelectorAll(`.${styles.left_detail_selected}`);
 
-      detailFilter.forEach(detailBtn => {
-        detailBtn.addEventListener('click', (e) => {
-          e.currentTarget.classList.toggle(`${styles.active}`);
-          e.currentTarget.childNodes[1].classList.toggle(`${styles.active}`);
-          //     e.currentTarget.classList.contains(`${styles.active}`) ? e.currentTarget.classList.remove(`${styles.active}`) : e.currentTarget.classList.add(`${styles.active}`); // 클릭한 버튼의 활성화 여부에 따라 active 클래스 추가 또는 삭제
-          // e.currentTarget.childNodes[1].classList.contains(`${styles.active}`) ? e.currentTarget.childNodes[1].classList.remove(`${styles.active}`) : e.currentTarget.childNodes[1].classList.add(`${styles.active}`); // 클릭한 버튼 > 필터링 리스트의 active 클래스 추가 또는 삭제
-        });
+      // 오른쪽 필터링 리스트 선택 필터
+      const selectedRightOption = document.querySelectorAll(`.${styles.right_detail_selected}`);
+
+
+      detailFilter.forEach(detailBtn => { //디테일 필터 각각에
+        detailBtn.addEventListener('click', (e) => { // 클릭 이벤트 추가
+          e.currentTarget.classList.contains(`${styles.active}`) ? e.currentTarget.classList.remove(`${styles.active}`) : e.currentTarget.classList.add(`${styles.active}`); // 클릭한 버튼의 활성화 여부에 따라 active 클래스 추가 또는 삭제
+          e.currentTarget.childNodes[1].classList.contains(`${styles.active}`) ? e.currentTarget.childNodes[1].classList.remove(`${styles.active}`) : e.currentTarget.childNodes[1].classList.add(`${styles.active}`); // 클릭한 버튼 > 필터링 리스트의 active 클래스 추가 또는 삭제
+        })
       });
 
-      // detailFilterItem.forEach(detailItem => { // 디테일 필터링 리스트 아이템 각각에
-      //   detailItem.addEventListener('click', (e) => { // 클릭 이벤트 추가
-      //     e.currentTarget.closest("ul").classList.remove(`.${styles.active}`); // 디테일 필터링 리스트 비활성화
-      //     console.log("현재 클릭한 필터 : " + e.currentTarget.innerText);
-      //     // 해당 카테고리에 맞는 cultureList 필터링
-      //     let filteredList = [];
-          
-      //     if (e.currentTarget.innerText === "마감임박순") { // 마감임박순
-      //       filteredList = cultureListObj.perforList.sort((a,b) => {return Number(a.endDate) - Number(b.endDate)});
-      //       setSubFilter(e.currentTarget.innerText);            
-      //     } else if(e.currentTarget.innerText === "최신등록순"){ // 최신등록순
-      //       filteredList = cultureListObj.perforList.sort((a,b) => {return Number(b.startDate) - Number(a.startDate)});
-      //       setSubFilter(e.currentTarget.innerText);
-      //       console.log("최신등록순 : " + JSON.stringify(cultureListObj));
-      //     } else if(e.currentTarget.innerText === "가격순"){ // 가격순
-      //       filteredList = cultureListObj.perforList.sort((a,b) => {return Number(b.startDate) - Number(a.startDate)});
-      //       setSubFilter(e.currentTarget.innerText);
-      //     } else if(e.currentTarget.innerText === "허니팟 많은순"){ // 허니팟 많은 순
-      //       setSubFilter(e.currentTarget.innerText);
-      //     } else if(e.currentTarget.innerText === "인기순"){ // 인기순(예매 많은 순)
-      //       setSubFilter(e.currentTarget.innerText);
-      //     } else if(e.currentTarget.innerText == "전체 지역"){ // 클릭한 필터가 전체 지역인 경우
-      //         filteredList = cultureListObj.perforList;
-      //         setAreaFilter("전체 지역");
-      //     }else{                
-      //       filteredList = cultureList.perforList.filter(item => item.area.match(e.currentTarget.innerText));
-      //       setAreaFilter(e.currentTarget.innerText);
-      //     }
+      detailFilterItem.forEach(detailItem => { // 디테일 필터링 리스트 아이템 각각에
+        detailItem.addEventListener('click', (e) => { // 클릭 이벤트 추가
+          e.currentTarget.closest("ul").classList.remove(`.${styles.active}`); // 디테일 필터링 리스트 비활성화
+          e.currentTarget.closest("ul").previousElementSibling.childNodes[0].innerText = e.currentTarget.innerText;
+          const filter = e.currentTarget.dataset.filter;
 
-      //     // 필터링된 결과를 cultureList 상태에 업데이트
-      //     setCultureList({ ...cultureListObj, perforList: filteredList });
-      //     setCurrentPage(1); // 페이지를 1로 초기화
-      //   })
-      // });
-      const handleDetailItemClick = (e) => {
-        e.currentTarget.closest("ul").classList.remove(`.${styles.active}`);
-        console.log("현재 클릭한 필터 : " + e.currentTarget.innerText);
-        console.log()
+          // 해당 카테고리에 맞는 cultureList 필터링
+          // let filteredList = [];
+          // if (filter === "dealine") { // 마감임박순
+          //   filteredList = cultureListObj.perforList; // 전체보기일 경우 전체 리스트 반환
+          // } else if(filter === "미술"){ // 전시
+          //   filteredList = cultureListObj.perforList.filter(item => item.realmName === filter || item.title.match("전시"));
+          // } else if(filter === "음악"){ // 공연
+          //   filteredList = cultureListObj.perforList.filter(item => item.realmName === genre || item.realmName === "연극" || item.title.match("음악") || item.title.match("영화"));
+          // } else if(genre === "뮤지컬"){ // 뮤지컬
+          //   filteredList = cultureListObj.perforList.filter(item => item.title.match("뮤지") || item.title.match("뮤지컬"));
+          // } else if(genre === "축제"){ // 행사 / 축제
+          //   filteredList = cultureListObj.perforList.filter(item => item.title.match("축제") || item.title.match("페스티벌"));
+          // }
 
-        let filteredList = [];
-
-        if (e.currentTarget.innerText === "마감임박순") {
-          filteredList = cultureList.perforList.sort((a, b) => Number(a.endDate) - Number(b.endDate));
-          setSubFilter(e.currentTarget.innerText);
-        } else if (e.currentTarget.innerText === "최신등록순") {
-          filteredList = cultureList.perforList.sort((a, b) => Number(b.startDate) - Number(a.startDate));
-          setSubFilter(e.currentTarget.innerText);
-        } else if (e.currentTarget.innerText === "가격순") {
-          filteredList = cultureListObj.perforList.sort((a, b) => Number(b.startDate) - Number(a.startDate));
-          setSubFilter(e.currentTarget.innerText);
-        } else if (e.currentTarget.innerText === "허니팟 많은순") {
-          setSubFilter(e.currentTarget.innerText);
-        } else if (e.currentTarget.innerText === "인기순") {
-          setSubFilter(e.currentTarget.innerText);
-        } else if (e.currentTarget.innerText === "전체 지역") {
-          filteredList = cultureListObj.perforList;
-          setAreaFilter("전체 지역");
-        } else {
-          filteredList = cultureListObj.perforList.filter(item => item.area.match(e.currentTarget.innerText));
-          setAreaFilter(e.currentTarget.innerText);
-        }
-
-        setCultureList({ ...cultureList, perforList: filteredList });
-        // setCultureList({ ...cultureListObj, perforList: filteredList });
-        setCurrentPage(1);
-      };
-
-      detailFilterItem.forEach(detailItem => {
-        detailItem.addEventListener('click', handleDetailItemClick);
+          // 필터링된 결과를 cultureList 상태에 업데이트
+          // setCultureList({ ...cultureListObj, perforList: filteredList });
+          // setCurrentPage(1); // 페이지를 1로 초기화
+        })
       });
 
 
@@ -217,28 +175,44 @@ export default function CultureInfo(props) {
           genreFilterList.forEach(genreBtn => {
             genreBtn.removeEventListener('click', () => {});
           });
-
-          detailFilter.forEach(detailBtn => { //디테일 필터 각각에
-            detailBtn.removeEventListener('click', () => {})
-          });
-          
-          detailFilterItem.forEach(detailItem => { // 디테일 필터링 리스트 아이템 각각에
-            detailItem.removeEventListener('click', () => {})
-          });
         };        
       }
     }, [props.cultureList]
   );
-
-  useEffect(() => {
-    console.log("cultureList : ", cultureList);
-  }, [cultureList]);
 
 
   useEffect(
     () => {
 
       window.scrollTo(0, 0); //페이지 이동시, 최상단으로 스크롤 위치
+
+      // 공연/전시 세부 필터
+      const detailFilter = document.querySelectorAll(`.${styles.detail_filter_list} > li`);
+
+      // 공연/전시 세부 필터링 리스트 아이템
+      const detailFilterItem = document.querySelectorAll(`.${styles.detail_filter_list} > li > ul > li`);
+
+      // 선택한 공연/전시 세부 필터링 리스트 아이템 텍스트
+      // 왼쪽 필터링 리스트 선택 필터
+      const selectedLeftOption = document.querySelectorAll(`.${styles.left_detail_selected}`);
+
+      // 오른쪽 필터링 리스트 선택 필터
+      const selectedRightOption = document.querySelectorAll(`.${styles.right_detail_selected}`);
+
+
+      detailFilter.forEach(detailBtn => { //디테일 필터 각각에
+        detailBtn.addEventListener('click', (e) => { // 클릭 이벤트 추가
+          e.currentTarget.classList.contains(`${styles.active}`) ? e.currentTarget.classList.remove(`${styles.active}`) : e.currentTarget.classList.add(`${styles.active}`); // 클릭한 버튼의 활성화 여부에 따라 active 클래스 추가 또는 삭제
+          e.currentTarget.childNodes[1].classList.contains(`${styles.active}`) ? e.currentTarget.childNodes[1].classList.remove(`${styles.active}`) : e.currentTarget.childNodes[1].classList.add(`${styles.active}`); // 클릭한 버튼 > 필터링 리스트의 active 클래스 추가 또는 삭제
+        })
+      });
+
+      detailFilterItem.forEach(detailItem => { // 디테일 필터링 리스트 아이템 각각에
+        detailItem.addEventListener('click', (e) => { // 클릭 이벤트 추가
+          e.currentTarget.closest("ul").classList.remove(`.${styles.active}`); // 디테일 필터링 리스트 비활성화
+          e.currentTarget.closest("ul").previousElementSibling.childNodes[0].innerText = e.currentTarget.innerText;
+      });
+    });
 
       // 공연/전시 카드/테이블 타입 보기 필터
       const viewFilter = document.querySelectorAll(`.${styles.view_filter_list} > li`);
@@ -285,6 +259,7 @@ export default function CultureInfo(props) {
   // 페이지 변경 함수
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
+    window.scrollTo(0, 0); // 페이지 이동시, 최상단으로 스크롤 위치
   };
 
   const Pagination = ({ items, itemsPerPage, currentPage, onPageChange }) => {
@@ -428,11 +403,11 @@ export default function CultureInfo(props) {
                 <p>행사/축제</p>
                 <span className={styles.filter_count}>{festivalCount}</span>
               </li>
-              <li className={styles.flex_center} data-genre="팝업">
+              <li className={styles.flex_center}>
                 <p>팝업</p>
                 <span className={styles.filter_count}>{popCount}</span>
               </li>
-              <li className={styles.flex_center} data-genre="얼리버드">
+              <li className={styles.flex_center}>
                 <p>얼리버드</p>
                 <span className={styles.filter_count}>{earlyCount}</span>
               </li>
@@ -444,36 +419,36 @@ export default function CultureInfo(props) {
             <div className={`${styles.filter_box} ${styles.flex_between}`}>
               <ul className={`${styles.detail_filter_list} ${styles.flex_start}`}>
                 <li className={styles.left_filter}>
-                  <span className={`${styles.selected_filter} ${styles.left} ${styles.flex_between}`}><span className={`${styles.selected_option} ${styles.left_detail_selected}`}>{subFilter}</span><img src={`${process.env.PUBLIC_URL}/images/commons/icon_arrow_bottom_main_color.png`} alt="arrow direction bottom icon" className={styles.filter_arrow_icon} /></span>
+                  <span className={`${styles.selected_filter} ${styles.left} ${styles.flex_between}`}><span className={`${styles.selected_option} ${styles.left_detail_selected}`}>최신등록순</span><img src={`${process.env.PUBLIC_URL}/images/commons/icon_arrow_bottom_main_color.png`} alt="arrow direction bottom icon" className={styles.filter_arrow_icon} /></span>
                   <ul>
                     <li data-filter="deadline">마감임박순</li>
                     <li data-filter="lately">최신등록순</li>
-                    <li data-filter="low">가격낮은순</li>
+                    {/* <li data-filter="low">가격낮은순</li> */}
                     <li data-filter="honeyPot">허니팟 많은순</li>
                     <li data-filter="hot">인기순</li>
                   </ul>
                 </li>
                 <li className={`${styles.right_filter} ${styles.region_filter}`}>
-                  <span className={`${styles.selected_filter} ${styles.right} ${styles.flex_between}`}><span className={`${styles.selected_option} ${styles.right_detail_selected}`}>{areaFilter}</span><img src={`${process.env.PUBLIC_URL}/images/commons/icon_arrow_bottom_main_color.png`} alt="arrow direction bottom icon" className={styles.filter_arrow_icon} /></span>
+                  <span className={`${styles.selected_filter} ${styles.right} ${styles.flex_between}`}><span className={`${styles.selected_option} ${styles.right_detail_selected}`}>전체 지역</span><img src={`${process.env.PUBLIC_URL}/images/commons/icon_arrow_bottom_main_color.png`} alt="arrow direction bottom icon" className={styles.filter_arrow_icon} /></span>
                   <ul>
-                    <li>전체 지역</li>
-                    <li>서울</li>
-                    <li>인천</li>
-                    <li>부산</li>
-                    <li>경기</li>
-                    <li>충북</li>
-                    <li>충남</li>
-                    <li>경남</li>
-                    <li>경북</li>
-                    <li>대구</li>
-                    <li>대전</li>
-                    <li>광주</li>
-                    <li>세종</li>
-                    <li>울산</li>
-                    <li>강원</li>
-                    <li>전남</li>
-                    <li>전북</li>
-                    <li>제주</li>
+                    <li data-region="all">전체 지역</li>
+                    <li data-region="sel">서울</li>
+                    <li data-region="ich">인천</li>
+                    <li data-region="bs">부산</li>
+                    <li data-region="gg">경기</li>
+                    <li data-region="chN">충북</li>
+                    <li data-region="chS">충남</li>
+                    <li data-region="kS">경남</li>
+                    <li data-region="kN">경북</li>
+                    <li data-region="tk">대구</li>
+                    <li data-region="dj">대전</li>
+                    <li data-region="gj">광주</li>
+                    <li data-region="sj">세종</li>
+                    <li data-region="ws">울산</li>
+                    <li data-region="gw">강원</li>
+                    <li data-region="jS">전남</li>
+                    <li data-region="jN">전북</li>
+                    <li data-region="jj">제주</li>
                   </ul>
                 </li>
               </ul>
