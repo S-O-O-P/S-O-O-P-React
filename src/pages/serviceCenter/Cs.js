4245b8f6bd2a00f05cee3102cb0d45bd62f98793
Bbini0104 <goqlsdl0120@gmail.com>
2024-07-12 10:15:28 +0900

    LINK 변경

--- src/pages/serviceCenter/Cs.js
+++ src/pages/serviceCenter/Cs.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import style from './Cs.module.css';
-import { useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { noticeAPI } from '../../apis/serviceCenter/CsNotice';
 
 function CsPage() {
@@ -30,32 +30,32 @@ function CsPage() {
                                 <img className={style.beeImg} src="/images/commons/icon_bee.png" alt="자주 묻는 질문" />
                                 <span className={style.subTitle}>자주 묻는 질문</span>
                             </span>
-                            <a href="/faq" className={style.linkButton}>더보기&gt;</a>
+                            <NavLink to={"/faq"} className={style.linkButton}>더보기&gt;</NavLink>
                         </div>
                         <div className={style.listBox}>
                             <div className={style.faq}>
                                 <img className={style.faqBeeImg} src="/images/commons/icon_bee.png" alt="자주 묻는 질문 아이콘"></img>
-                                <a href="/faq" className={style.faqContent}>링크비(LINKBEE)는 어떤 서비스인가요?</a>
+                                <NavLink to={"/faq"} className={style.faqContent}>링크비(LINKBEE)는 어떤 서비스인가요?</NavLink>
                             </div>
                             <hr className={style.hrLine} />
                             <div className={style.faq}>
                                 <img className={style.faqBeeImg} src="/images/commons/icon_bee.png" alt="자주 묻는 질문 아이콘"></img>
-                                <a href="/faq" className={style.faqContent}>링크비에 가입하려면 어떻게 해야 하나요?</a>
+                                <NavLink to={"/faq"} className={style.faqContent}>링크비에 가입하려면 어떻게 해야 하나요?</NavLink>
                             </div>
                             <hr className={style.hrLine} />
                             <div className={style.faq}>
                                 <img className={style.faqBeeImg} src="/images/commons/icon_bee.png" alt="자주 묻는 질문 아이콘"></img>
-                                <a href="/faq" className={style.faqContent}>링크비를 통해 어떤 공연과 전시 정보를 얻을 수 있나요?</a>
+                                <NavLink to={"/faq"} className={style.faqContent}>링크비를 통해 어떤 공연과 전시 정보를 얻을 수 있나요?</NavLink>
                             </div>
                             <hr className={style.hrLine} />
                             <div className={style.faq}>
                                 <img className={style.faqBeeImg} src="/images/commons/icon_bee.png" alt="자주 묻는 질문 아이콘"></img>
-                                <a href="/faq" className={style.faqContent}>티켓은 어떻게 구매할 수 있나요?</a>
+                                <NavLink to={"/faq"} className={style.faqContent}>티켓은 어떻게 구매할 수 있나요?</NavLink>
                             </div>
                             <hr className={style.hrLine} />
                             <div className={style.faq}>
                                 <img className={style.faqBeeImg} src="/images/commons/icon_bee.png" alt="자주 묻는 질문 아이콘"></img>
-                                <a href="/faq" className={style.faqContent}>링크비의 매칭 서비스는 어떻게 작동하나요?</a>
+                                <NavLink to={"/faq"} className={style.faqContent}>링크비의 매칭 서비스는 어떻게 작동하나요?</NavLink>
                             </div>
                         </div>
                     </div>
@@ -63,14 +63,14 @@ function CsPage() {
                     <div className={style.box}>
                         <div className={style.subTitleBox}>
                             <span className={style.subTitle}>공지사항</span>
-                            <a href="/notice" className={style.linkButton}>더보기&gt;</a>
+                            <NavLink to={"/notice"} className={style.linkButton}>더보기&gt;</NavLink>
                         </div>
                         <div className={style.listBox}>
                             {notices.map((notice, index) => (
                                 <ul key={notice.noticeCode}>
-                                    <a href={`/notice/${notice.noticeCode}`}>
+                                    <NavLink to={`/notice/${notice.noticeCode}`}>
                                         <li className={style.faqContent}>{notice.title}</li>
-                                    </a>
+                                    </NavLink>
                                     {index < notices.length - 1 && <hr className={style.hrLine} />}
                                 </ul>
                             ))}
